6c89ef923c378df180951dce88326fa9aa56745c
Magnus Tingberg <mtingberg@outlook.com>
2015-05-28 16:27:10 +0200

    Update current weather controller to use forecast service.

--- app/current-weather/current-weather-controller.js
+++ app/current-weather/current-weather-controller.js
@@ -1,16 +1,9 @@
 'use strict';
 
-var angular = require('angular'),
-    //apiKey = 'f8dc9e2b6acd93f0a57b4abf0e86ecad',
-    lund = 'http://api.openweathermap.org/data/2.5/weather?q=Lund,se';
+var angular = require('angular');
 
-angular.module('app').controller('CurrentWeatherCtrl', function ($scope, $http) {
-    $http.get(lund).
-        success(function (data) {
-            $scope.weatherData = data;
-            console.log(data);
-        }).
-        error(function () {
-            console.error('Lookup of \'' + lund + '\' failed');
-        });
+angular.module('app').controller('CurrentWeatherCtrl', function (forecastFactory) {
+    forecastFactory.getForecastForPosition({'longitude': 13.19, 'latitude': 55.71}).then(function (data) {
+        console.log('CurrentWeatherCtrl', data);
+    });
 });
