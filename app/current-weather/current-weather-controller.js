f508cb41228c4f7a4e2b589b2b5fc9b8c811155f
Magnus Tingberg <mtingberg@outlook.com>
2015-05-29 17:30:34 +0200

    Update current weather feature:
    * Add geolocation lookup.
    * Change view into angular template.
    * Add styling.

--- app/current-weather/current-weather-controller.js
+++ app/current-weather/current-weather-controller.js
@@ -1,9 +1,32 @@
 'use strict';
 
-var angular = require('angular');
+var angular = require('angular'),
+    lookupWeatherIcon = require('../lookup-weather-icon'),
+    i18nIsoCountries = require('i18n-iso-countries');
 
-angular.module('app').controller('CurrentWeatherCtrl', function (forecastFactory) {
-    forecastFactory.getForecastForPosition({'longitude': 13.19, 'latitude': 55.71}).then(function (data) {
-        console.log('CurrentWeatherCtrl', data);
+
+angular.module('app').controller('CurrentWeatherCtrl', function ($scope, forecastFactory) {
+    new Promise(function (resolve, reject) {
+
+        function success(position) {
+            resolve({latitude: position.coords.latitude, longitude: position.coords.longitude});
+        }
+
+        function error() {
+            reject('Unable to retrieve location');
+        }
+
+        navigator.geolocation.getCurrentPosition(success, error);
+
+    }).then(function (position) {
+        forecastFactory.getCurrentWeatherForPosition(position).then(function (data) {
+            $scope.forecast = {
+                weatherIcon: lookupWeatherIcon(data.weather[0].icon),
+                city: data.name,
+                country: i18nIsoCountries.getName(data.sys.country, 'en'),
+                currentTemperature: Math.round(parseInt(data.main.temp, 10)),
+                temperatureUnit: 'C'
+            };
+        });
     });
 });
