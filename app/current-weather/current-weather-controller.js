99b093969d370b2af22608f542d971e63405153f
Magnus Tingberg <mtingberg@outlook.com>
2015-06-02 17:06:50 +0200

    Update currentWeatherController:
    * Introduce intermediate step to get the correct location name.
    * Change for calling improved weather icon lookup functionality.

--- app/current-weather/current-weather-controller.js
+++ app/current-weather/current-weather-controller.js
@@ -2,6 +2,7 @@
 
 var angular = require('angular'),
     lookupWeatherIcon = require('../lookup-weather-icon'),
+    isDayTime = require('../is-day-time'),
     i18nIsoCountries = require('i18n-iso-countries');
 
 
@@ -19,10 +20,28 @@ module.exports = angular.module('app').controller('CurrentWeatherCtrl', function
         navigator.geolocation.getCurrentPosition(success, error);
 
     }).then(function (position) {
-        forecastFactory.getCurrentWeatherForPosition(position).then(function (data) {
+        // This step is a workaround due to that the OpenWeatherMap api's 'current weather forecast'
+        // returns the location with two decimals precision only, and hence retrieves an incorrect
+        // (but nearby) location in some cases.
+        // Calling OpenWeatherMap 'daily forecast' is used since this api call retrieves the location
+        // with a higher precision (i.e. uses full number of decimals returned from
+        // navigator.geolocation.getCurrentPosition() ).
+
+        return forecastFactory.getDailyForecastForPosition(position).then(function (data) {
+            return {
+                position: position,
+                city: data.city.name
+            };
+        });
+
+    }).then(function (locationInfo) {
+        forecastFactory.getCurrentWeatherForPosition(locationInfo.position).then(function (data) {
+            var weatherConditionCode = data.weather[0].id,
+                isDayTimeAtLocation = isDayTime(data.weather[0].icon);
+
             $scope.forecast = {
-                weatherIcon: lookupWeatherIcon(data.weather[0].icon),
-                city: data.name,
+                weatherIcon: lookupWeatherIcon(weatherConditionCode, isDayTimeAtLocation),
+                city: locationInfo.city,
                 country: i18nIsoCountries.getName(data.sys.country, 'en'),
                 currentTemperature: Math.round(parseInt(data.main.temp, 10)),
                 temperatureUnit: 'C'
